990727e76552900e5b14b7e8b2e6c285437b33e5
whejy <john.wheldale@gmail.com>
2024-04-05 15:29:05 +1030

    e2e test for individual pokemon

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -1,11 +1,16 @@
 // @ts-check
-// @ts-ignore
-const { test, describe, expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
-describe('Pokedex', () => {
+test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('individual pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('oddish').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
