ff446009a13c73cd97d47a9ea26c5cdde8e39edc
whejy <john.wheldale@gmail.com>
2024-04-08 14:40:33 +0930

    add health check to workflow

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
