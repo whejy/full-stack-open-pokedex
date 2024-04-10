0d97d69fe3531db120db96de8e2545ca9d72e6ce
whejy <john.wheldale@gmail.com>
2024-04-10 13:40:16 +0930

    test main branch protection

--- app.js
+++ app.js
@@ -16,6 +16,5 @@ app.get('/health', (req, res) => {
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
